41bc3b521c3a1efe4fb9033a0713607c2eaf0f20
pradyotsrivastava <pradyotsrivastava93@gmail.com>
2024-06-08 03:33:31 +0530

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+import { Work_Sans } from "next/font/google";
+
+import "./globals.css";
+import { TooltipProvider } from "@/components/ui/tooltip";
+
+import Room from "./Room";
+
+export const metadata = {
+  title: "Sigma Clone",
+  description:
+    "A minimalist Figma clone using fabric.js and Liveblocks for realtime collaboration",
+};
+
+const workSans = Work_Sans({
+  subsets: ["latin"],
+  variable: "--font-work-sans",
+  weight: ["400", "600", "700"],
+});
+
+const RootLayout = ({ children }: { children: React.ReactNode }) => (
+  <html lang='en'>
+    <body
+      className={`${workSans.className} min-h-[calc(100vh-180px)] bg-primary-grey-200`}
+    >
+      <Room>
+        <TooltipProvider>{children}</TooltipProvider>
+      </Room>
+    </body>
+  </html>
+);
+
+export default RootLayout;
